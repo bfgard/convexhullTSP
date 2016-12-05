a5dd45b4e0cc40cded6e6669adcfc977d150097c
mormonfro <5164541@gmail.com>
2016-12-05 16:12:56 -0700

    made progress on combining convex hulls.

---
+++ TSP/TSP/WindowsFormsApplication1/CombineState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP
+{
+    class CombineState
+    {
+        public CombineState(CombineState state)
+        {
+            this.citiesOuter = state.citiesOuter;
+            this.citiesInner = state.citiesInner;
+            this.visable = state.visable;
+            this.i_inner = state.i_inner;
+            this.i_outer = state.i_outer;
+            this.on_outer = state.on_outer;
+            this.length = state.length;
+        }
+        public CombineState(City[] citiesOuter, City[] citiesInner, Tuple<City, City>[] visable, int i_inner, int i_outer, bool on_outer, double length)
+        {
+            this.citiesOuter = citiesOuter;
+            this.citiesInner = citiesInner;
+            this.visable = visable;
+            this.i_inner = i_inner;
+            this.i_outer = i_outer;
+            this.on_outer = on_outer;
+            this.length = length;
+        }
+
+        public City[] citiesOuter;
+        public City[] citiesInner;
+        public Tuple<City, City>[] visable;
+        public int i_inner;
+        public int i_outer;
+        public bool on_outer;
+        public double length;
+    }
+}
--- TSP/TSP/WindowsFormsApplication1/ConvexHullTSP.cs
+++ TSP/TSP/WindowsFormsApplication1/ConvexHullTSP.cs
@@ -32,8 +32,180 @@ namespace TSP
         // returns minimum combination of two hulls (Roy)
         City[] CombineHulls(City[] citiesOuter, City[] citiesInner, Tuple<City, City>[] visable)
         {
-            // FIXME implement
-            return null;
+            City[] combineHull = new City[citiesOuter.Length + citiesInner.Length];
+
+            //indecise the inner/outer/combine hull
+            int i_outer = 0;
+            int i_inner = -1;
+            int i_combine = 0;
+
+            combineHull[i_combine++] = citiesOuter[i_outer];
+
+            while(i_inner == -1)
+            {
+                // find the closest visable point from citiesOuter[0]
+                int[] local_visable = FindVisableFromOuter(citiesOuter[i_outer], citiesInner, visable);
+                
+                double min = double.MaxValue;
+                foreach(int lv in local_visable) {
+                    double next = citiesOuter[i_outer].costToGetTo(citiesInner[lv]);
+                    if(next < min)
+                    {
+                        min = next;
+                        i_inner = lv;
+                    }
+                }
+                // if there are no visable points then it moves to the next point in citiesOuter
+                if (i_inner == -1)
+                {
+                    combineHull[i_combine++] = citiesOuter[++i_outer];
+                }
+            }
+
+            bool on_outer = true;
+
+            while(true)
+            {
+                const int lookahead = 1;
+                // find the best connection locally with lookahead + 1 look aheads.
+                // the final '+ 1' local ahead always assumes connection between the hulls
+                CombineState initial = new CombineState(i_inner, i_outer, on_outer, 0);
+
+                double across = goAcross(initial, lookahead);
+                double around = goAround(initial, lookahead);
+                if(across < around)
+                {
+                    // go across
+                    if(on_outer)
+                    {
+                        combineHull[i_combine++] = citiesInner[++i_inner];
+                    }
+                    else
+                    {
+                        combineHull[i_combine++] = citiesOuter[++i_outer];
+                    }
+                    on_outer = !on_outer;
+                }
+                else
+                {
+                    // go around
+                    if (!on_outer)
+                    {
+                        combineHull[i_combine++] = citiesInner[++i_inner];
+                    }
+                    else
+                    {
+                        combineHull[i_combine++] = citiesOuter[++i_outer];
+                    }
+                }
+            }
+            
+            return combineHull;
+        }
+
+        double goAcross(CombineState state, int depth)
+        {
+            //FIXME think about when the loop is almost done
+            //FIXME think about if it is not visable
+            if (state.on_outer)
+            {
+                int[] local_visable = FindVisableFromOuter(state.citiesOuter[state.i_outer], state.citiesInner, state.visable);
+                if (local_visable.Contains(state.i_inner))
+                {
+                    state.length += state.citiesOuter[state.i_outer].costToGetTo(state.citiesInner[state.i_inner]);
+                    state.on_outer = false;
+                    
+                }
+            }
+            else
+            {
+                int[] local_visable = FindVisableFromOuter(state.citiesInner[state.i_inner], state.citiesOuter, state.visable);
+                if (local_visable.Contains(state.i_outer))
+                {
+                    state.length += state.citiesInner[state.i_inner].costToGetTo(state.citiesOuter[state.i_outer]);
+                    state.on_outer = true;
+                }
+            }
+            if (depth < 0)
+            {
+                return state.length;
+            } else if (depth < 1)
+            {
+                return goAcross(new CombineState(state), depth - 1),
+            }
+            else
+            {
+                return Math.Min(
+                    goAcross(new CombineState(state), depth - 1),
+                    goArround(new CombineState(state), depth - 1));
+            }
+        }
+
+        double goArround(CombineState state, int depth)
+        {
+            //FIXME think about when the loop is almost done
+            if (state.on_outer)
+            {
+                state.length += state.citiesOuter[state.i_outer].costToGetTo(state.citiesOuter[ModInc(state.i_outer, state.citiesOuter.Length)]);
+            }
+            else
+            {
+                state.length += state.citiesInner[state.i_inner].costToGetTo(state.citiesInner[ModInc(state.i_inner, state.citiesInner.Length)]);
+            }
+
+            if (depth < 1)
+            {
+                return goAcross(new CombineState(state), depth - 1);
+            }
+            else
+            {
+                return Math.Min(
+                    goAcross(new CombineState(state), depth - 1),
+                    goArround(new CombineState(state), depth - 1));
+            }
+        }
+
+        int ModInc(int i, int n)
+        {
+            return (i + 1) % n;
+        }
+
+        int[] FindVisableFromOuter(City cityOuter, City[] citiesInner, Tuple<City, City>[] visable)
+        {
+            List<int> result = new List<int>();
+            for(int i = 0; i < visable.Length; i++)
+            {
+                if(visable[i].Item1 == cityOuter)
+                {
+                    for(int j = 0; j < citiesInner.Length; j++)
+                    {
+                        if(visable[i].Item2 == citiesInner[j])
+                        {
+                            result.Add(j);
+                        }
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        int[] FindVisableFromInner(City cityInner, City[] citiesOuter, Tuple<City, City>[] visable)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < visable.Length; i++)
+            {
+                if (visable[i].Item2 == cityInner)
+                {
+                    for (int j = 0; j < citiesOuter.Length; j++)
+                    {
+                        if (visable[i].Item1 == citiesOuter[j])
+                        {
+                            result.Add(j);
+                        }
+                    }
+                }
+            }
+            return result.ToArray();
         }
 
         City[] run(City[] cities)
