227325e684be9678f68780a7371deaa508a233d5
mormonfro <5164541@gmail.com>
2016-12-05 14:20:38 -0700

    I <i>finished</i> the run meathod.  Tell me if you have any questions.

--- TSP/TSP/WindowsFormsApplication1/ConvexHullTSP.cs
+++ TSP/TSP/WindowsFormsApplication1/ConvexHullTSP.cs
@@ -38,9 +38,27 @@ namespace TSP
 
         City[] run(City[] cities)
         {
-            // FIXME implement
-            return null;
-        }
+            List<City[]> hulls = new List<City[]>();
 
+            City[] remaining = cities;
+            while(remaining.Length > 0)
+            {
+                hulls.Add( FindConvexHull(remaining) );
+                remaining = SubtractCities(remaining, hulls[hulls.Count - 1]);
+            }
+
+            // keeping the combine hull in hull[0] and continuing to combine with each other 1 -> N
+            for (int i = 1; i < hulls.Count; i++)
+            {
+                hulls[0] = 
+                    CombineHulls(
+                        hulls[0],
+                        hulls[i],
+                        FindVisablePoints(hulls[0], hulls[i])
+                        );
+            }
+
+            return hulls[0];
+        }
     }
 }
